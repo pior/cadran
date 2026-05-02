fef3aa8ad838a5a53ddefabe459c90474cea9dc1
Pior Bastida <pior@pbastida.net>
2026-05-02 20:09:36 +0200

    Timezone: fix relative day logic and add unit tests

--- src/timezone.rs
+++ src/timezone.rs
@@ -50,18 +50,52 @@ impl TimezoneEntry {
 }
 
 fn relative_day_label(now: &Zoned, target: &Zoned) -> &'static str {
-    let now_at_target = now.with_time_zone(target.time_zone().clone());
-    let date_now = now_at_target.date();
+    let date_now = now.date();
     let date_target = target.date();
 
     if date_target == date_now {
         "Today"
-    } else if date_target == date_now.tomorrow().expect("valid date") {
+    } else if date_target > date_now {
         "Tomorrow"
-    } else if date_target == date_now.yesterday().expect("valid date") {
-        "Yesterday"
     } else {
-        ""
+        "Yesterday"
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use jiff::civil::date;
+    use jiff::tz::TimeZone;
+
+    #[test]
+    fn test_relative_day_label() {
+        let utc = TimeZone::UTC;
+        let tokyo = TimeZone::get("Asia/Tokyo").unwrap();
+        let ny = TimeZone::get("America/New_York").unwrap();
+
+        // Local time: 2024-05-02 12:00:00 UTC
+        let now = date(2024, 5, 2).at(12, 0, 0, 0).to_zoned(utc).unwrap();
+
+        // Target Tokyo: 2024-05-02 21:00:00 (Same day)
+        let target_tokyo = now.with_time_zone(tokyo.clone());
+        assert_eq!(relative_day_label(&now, &target_tokyo), "Today");
+
+        // Target NY: 2024-05-02 08:00:00 (Same day)
+        let target_ny = now.with_time_zone(ny.clone());
+        assert_eq!(relative_day_label(&now, &target_ny), "Today");
+
+        // Late night UTC: 2024-05-02 22:00:00 UTC
+        let late_now = date(2024, 5, 2).at(22, 0, 0, 0).to_zoned(TimeZone::UTC).unwrap();
+        // Tokyo is now 2024-05-03 07:00:00 (Tomorrow)
+        let target_tokyo_tmw = late_now.with_time_zone(tokyo);
+        assert_eq!(relative_day_label(&late_now, &target_tokyo_tmw), "Tomorrow");
+
+        // Early morning UTC: 2024-05-02 02:00:00 UTC
+        let early_now = date(2024, 5, 2).at(2, 0, 0, 0).to_zoned(TimeZone::UTC).unwrap();
+        // NY is still 2024-05-01 22:00:00 (Yesterday)
+        let target_ny_yest = early_now.with_time_zone(ny);
+        assert_eq!(relative_day_label(&early_now, &target_ny_yest), "Yesterday");
     }
 }
 
